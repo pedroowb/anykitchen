2e2d5d25a9f0956e4e891e69a8e7789b8a81de36
pedroowb <phsilvasantos7@gmail.com>
2025-04-10 10:51:46 -0300

    feat(frontend): adicionar Bootstrap 5 ao projeto

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:admin] = true
-    redirect_to dishes_path, notice: "Login realizado com sucesso!"
+    if params[:email] == "admin" && params[:password] == "admin@2025"
+      session[:admin] = true
+      redirect_to dishes_path, notice: "Login realizado com sucesso!"
+    else
+      flash.now[:alert] = "Credenciais inválidas"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
