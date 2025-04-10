3c7e4379ec9f538de3c2a7f5c001bd6731ab9a9b
PEDRO HENRIQUE DA SILVA SANTOS <12101863@hsm.int>
2025-04-10 09:37:34 -0300

    feat(helper): adiciona helpers para validação de sessão e formatação de valores

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def admin?
+    session[:admin]
+  end
+
+  def format_price(price)
+    number_to_currency(price, unit: "R$", separator: ",", delimiter: ".")
+  end
 end
